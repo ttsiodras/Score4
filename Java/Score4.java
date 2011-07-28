1695d28ab2b69ba81ebfd77d0b32cbb4278b39d8
Thanassis Tsiodras <ttsiodras@gmail.com>
2011-07-28 17:42:11 +0300

    Ported optimizations from F#/C#/OCaml

--- Java/Score4.java
+++ Java/Score4.java
@@ -9,26 +9,20 @@ public class Score4 {
     static int g_maxDepth = 7;
     static boolean g_debug = false;
 
-    static enum Mycell {
-        Orange,
-        Yellow,
-        Barren;
-    }
-
     static class Board {
 
         final int width;
         final int height;
-        Mycell[][] slots;
+        int[][] slots;
 
         Board(int width, int height) {
-            slots = new Mycell[height][width];
+            slots = new int[height][width];
             this.width = width;
             this.height = height;
-            fill(Mycell.Barren);
+            fill(0);
         }
         
-        private void fill(Mycell type) {
+        private void fill(int type) {
             for (int j = 0; j < this.height; j++) {
                 for (int i = 0; i < this.width; i++) {
                     slots[j][i] = type;
@@ -40,9 +34,9 @@ public class Score4 {
             return y >= 0 && y < this.height && x >= 0 && x < this.width;
         }
 
-        int dropDisk(int column, Mycell color) {
+        int dropDisk(int column, int color) {
             for (int y = this.height - 1; y >= 0; y--) {
-                if (slots[y][column] == Mycell.Barren) {
+                if (slots[y][column] == 0) {
                     slots[y][column] = color;
                     return y;
                 }
@@ -50,7 +44,7 @@ public class Score4 {
             return -1;
         }
 
-        void load(Integer[] x, Integer[] y, Mycell[] color) {
+        void load(Integer[] x, Integer[] y, Integer[] color) {
             assert ((x.length == y.length) && (x.length == color.length));
             for (int i = 0; i < x.length; i++) {
                 slots[y[i]][x[i]] = color[i];
@@ -69,10 +63,10 @@ public class Score4 {
             System.out.println("You win");
             System.exit(-1);
         } else {
-            ScoreMoveTuple smt = abMinimax(true, Mycell.Orange, g_maxDepth, board);
+            ScoreMoveTuple smt = abMinimax(true, 1, g_maxDepth, board);
             if (smt.move != -1) {
                 System.out.println(smt.move);
-                board.dropDisk(smt.move, Mycell.Orange);
+                board.dropDisk(smt.move, 1);
                 scoreOrig = scoreBoard(board);
                 if (scoreOrig == orangeWins) {
                     System.out.println("I win");
@@ -92,19 +86,19 @@ public class Score4 {
 
     private static Board prepareBoard(String[] args) {
         Board board = new Board(width, height);
-        ArrayList<Mycell> colors = new ArrayList<Mycell>(9);
+        ArrayList<Integer> colors = new ArrayList<Integer>(9);
         ArrayList<Integer> x = new ArrayList<Integer>(9);
         ArrayList<Integer> y = new ArrayList<Integer>(9);
-        int i = 0;
+        Integer i = 0;
         for (String arg : args) {
             char[] cargs = arg.toCharArray();
             if (arg.length() == 3) {
                 if (cargs[0] == 'o') {
-                    colors.add(Mycell.Orange);
+                    colors.add(1);
                     y.add(cargs[1] - '0');
                     x.add(cargs[2] - '0');
                 } else if (cargs[0] == 'y') {
-                    colors.add(Mycell.Yellow);
+                    colors.add(-1);
                     y.add(cargs[1] - '0');
                     x.add(cargs[2] - '0');
                 }
@@ -115,51 +109,29 @@ public class Score4 {
             }
             i++;
         }
-        board.load(x.toArray(new Integer[x.size()]), y.toArray(new Integer[y.size()]), colors.toArray(new Mycell[colors.size()]));
+        board.load(x.toArray(new Integer[x.size()]), y.toArray(new Integer[y.size()]), colors.toArray(new Integer[colors.size()]));
         return board;
     }
 
     private static int scoreBoard(Board board) {
-        int[][] scores = new int[height][width];
         int[] counters = {0, 0, 0, 0, 0, 0, 0, 0, 0};
-        final int[][] negativeSlope = {{0, 0}, {1, 1}, {2, 2}, {3, 3}};
-        final int[][] positiveSlope = {{0, 0}, {-1, 1}, {-2, 2}, {-3, 3}};
 
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-                switch (board.slots[y][x]) {
-                    case Orange:
-                        scores[y][x] = 1;
-                        break;
-                    case Yellow:
-                        scores[y][x] = -1;
-                        break;
-                    case Barren:
-                        scores[y][x] = 0;
-                        break;
-                }
-            }
-        }
         // Horizontal spans
         for (int y = 0; y < height; y++) {
-            int score = scores[y][0] + scores[y][1] + scores[y][2];
+            int score = board.slots[y][0] + board.slots[y][1] + board.slots[y][2];
             for (int x = 3; x < width; x++) {
-                assert (board.inside(y, x));
-                score += scores[y][x];
+                score += board.slots[y][x];
                 counters[score + 4]++;
-                assert (board.inside(y, x - 3));
-                score -= scores[y][x - 3];
+                score -= board.slots[y][x - 3];
             }
         }
         // Vertical spans
         for (int x = 0; x < width; x++) {
-            int score = scores[0][x] + scores[1][x] + scores[2][x];
+            int score = board.slots[0][x] + board.slots[1][x] + board.slots[2][x];
             for (int y = 3; y < height; y++) {
-                assert (board.inside(y, x));
-                score += scores[y][x];
+                score += board.slots[y][x];
                 counters[score + 4]++;
-                assert (board.inside(y - 3, x));
-                score -= scores[y - 3][x];
+                score -= board.slots[y - 3][x];
             }
         }
         // Down-right (and up-left) diagonals
@@ -167,10 +139,7 @@ public class Score4 {
             for (int x = 0; x < width - 3; x++) {
                 int score = 0;
                 for (int idx = 0; idx < 4; idx++) {
-                    int yy = y + negativeSlope[idx][0];
-                    int xx = x + negativeSlope[idx][1];
-                    assert (board.inside(yy, xx));
-                    score += scores[yy][xx];
+                    score += board.slots[y+idx][x+idx];
                 }
                 counters[score + 4]++;
             }
@@ -180,10 +149,7 @@ public class Score4 {
             for (int x = 0; x < width - 3; x++) {
                 int score = 0;
                 for (int idx = 0; idx < 4; idx++) {
-                    int yy = y + positiveSlope[idx][0];
-                    int xx = x + positiveSlope[idx][1];
-                    assert (board.inside(yy, xx));
-                    score += scores[yy][xx];
+                    score += board.slots[y-idx][x+idx];
                 }
                 counters[score + 4]++;
             }
@@ -199,7 +165,7 @@ public class Score4 {
         }
     }
 
-    private static ScoreMoveTuple abMinimax(boolean maximizeOrMinimize, Mycell color, int depth, Board board) {
+    private static ScoreMoveTuple abMinimax(boolean maximizeOrMinimize, int color, int depth, Board board) {
 
         if (0 == depth) {
             ScoreMoveTuple smt = new ScoreMoveTuple();
@@ -210,7 +176,7 @@ public class Score4 {
             int bestScore = maximizeOrMinimize ? -10000000 : 10000000;
             int bestMove = -1;
             for (int column = 0; column < width; column++) {
-                if (board.slots[0][column] != Mycell.Barren) {
+                if (board.slots[0][column] != 0) {
                     continue;
                 }
                 int rowFilled = board.dropDisk(column, color);
@@ -221,15 +187,14 @@ public class Score4 {
                 if (s == (maximizeOrMinimize ? orangeWins : yellowWins)) {
                     bestMove = column;
                     bestScore = s;
-                    board.slots[rowFilled][column] = Mycell.Barren;
+                    board.slots[rowFilled][column] = 0;
                     break;
                 }
-                Integer moveInner, scoreInner;
-                ScoreMoveTuple inner = abMinimax(!maximizeOrMinimize, color == Mycell.Orange ? Mycell.Yellow : Mycell.Orange, depth - 1, board);
-                assert (inner != null);
+                int moveInner, scoreInner;
+                ScoreMoveTuple inner = abMinimax(!maximizeOrMinimize, -color, depth - 1, board);
                 moveInner = inner.move;
                 scoreInner = inner.score;
-                board.slots[rowFilled][column] = Mycell.Barren;
+                board.slots[rowFilled][column] = 0;
                 if ((depth == g_maxDepth) && g_debug) {
                     System.out.printf("Depth %d, placing on %d, score:%d\n", depth, column, scoreInner);
                 }
@@ -254,7 +219,7 @@ public class Score4 {
 
     static class ScoreMoveTuple {
 
-        public Integer score;
-        public Integer move;
+        public int score;
+        public int move;
     }
 }
