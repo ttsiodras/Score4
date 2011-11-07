351c52183ed4c78b54ceba429c0c420fdb62338a
Thanassis Tsiodras <ttsiodras@gmail.com>
2011-11-07 18:48:32 +0200

    for very small variance, it blew up...

--- stats.py
+++ stats.py
@@ -14,7 +14,11 @@ while True:
 
 print "Average value:", total/n
 variance = (totalSq - total*total/n)/n
-print "Std deviation:", math.sqrt(variance)
+try:
+    v = math.sqrt(variance)
+    print "Std deviation:", v
+except:
+    print "Std deviation:", 0.0
 srted = sorted(allOfThem)
 print "Median:", srted[-1 + len(allOfThem)/2]
 print "Min %4.3f sec" % srted[0]
